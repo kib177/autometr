a59009e3c3a6698bdab3e840ad7ad756ab15d438
Kirill Brigi <78744452+kib177@users.noreply.github.com>
2026-06-15 11:35:47 +0300

    Update scroll.js

--- js/scroll.js
+++ js/scroll.js
@@ -1,18 +1,13 @@
-// Эффект сжатия шапки при прокрутке
 const header = document.querySelector('header');
-let lastScrollY = 0;
 
 function onScroll() {
-    const scrollY = window.scrollY;
-    if (scrollY > 50) {           // срабатывает после 50px прокрутки
+    if (window.scrollY > 50) {
         header.classList.add('scrolled');
     } else {
         header.classList.remove('scrolled');
     }
-    lastScrollY = scrollY;
 }
 
-// Оптимизация – запуск не чаще 60 fps
 window.addEventListener('scroll', () => {
     requestAnimationFrame(onScroll);
 });
