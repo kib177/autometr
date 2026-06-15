7d7457382fbbe60a47ab8e953481b4c749e3e43e
Kirill Brigi <78744452+kib177@users.noreply.github.com>
2026-06-15 13:38:56 +0300

    Update map.js

--- js/map.js
+++ js/map.js
@@ -3,14 +3,14 @@ ymaps.ready(initMap);
 
 function initMap() {
     var myMap = new ymaps.Map("map", {
-        center: [53.7845, 27.6415],
+        center: [53.778878, 27.648851],
         zoom: 16,
         controls: ['zoomControl', 'fullscreenControl']
     });
     
-    var myPlacemark = new ymaps.Placemark([53.7845, 27.6415], {
-        hintContent: 'Автокартель',
-        balloonContent: '<strong>Автокартель</strong><br>аг. Гатово, ул. Фрунзе, 8<br>Тел: +375 33 913-14-30'
+    var myPlacemark = new ymaps.Placemark([53.778878, 27.648851], {
+        hintContent: 'АвтоМэтр СТО',
+        balloonContent: '<strong>АвтоМэтр СТО</strong><br>аг. Гатово, ул. Фрунзе, 8<br>Тел: +375 33 913-14-30'
     }, {
         preset: 'islands#blueAutoIcon'
     });
